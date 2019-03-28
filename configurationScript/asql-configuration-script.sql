452d105cefd5e8112fd599d209e5e6b3f398aa7a
Brendan Rushing <brushing@outlook.com>
2019-03-27 22:03:40 -0400

    script

--- configurationScript/asql-configuration-script.sql
+++ configurationScript/asql-configuration-script.sql
@@ -3,23 +3,24 @@ CREATE DATABASE asqlKanban;
 
 USE asqlKanban;
 
-DROP TABLE IF EXISTS Worker;
-CREATE TABLE Worker (
-	PK_workerType NVARCHAR(5) NOT NULL PRIMARY KEY,
-	skillLevel DECIMAL(3,2) NOT NULL,
-	assemblyTime DECIMAL(5,2) NOT NULL
-);
 
-DROP TABLE IF EXISTS ConfigurationTable;
+--  DROP TABLE IF EXISTS ConfigurationTable;
 CREATE TABLE ConfigurationTable (
-	PK_configurationID INT IDENTITY(1,1) PRIMARY KEY,
-	FK_workerType NVARCHAR(5) REFERENCES Worker(PK_workerType),
+	PK_configurationID INT IDENTITY(1,1) PRIMARY KEY,	
 	harnessQuantity INT NOT NULL,
 	reflectorQuantity INT NOT NULL,
 	housingQuantity INT NOT NULL,
 	lensQuantity INT NOT NULL,
 	bulbQuantity INT NOT NULL,
+	bezelQuantity INT NOT NULL,
 	runnerCollectionTimeCycle INT NOT NULL,
 	numberOfStations INT NOT NULL,
-	testTraySize INT NOT NULL
+	testTraySize INT NOT NULL,
+	rookieDefectRate DECIMAL(10,2) NOT NULL,
+	normalDefectRate DECIMAL(10,2) NOT NULL,
+	experiencedDefectRate DECIMAL(10,2) NOT NULL,
+	rookieAssemblyTime INT NOT NULL,
+	normalAssemblyTime INT NOT NULL,
+	experiencedAssemblyTime INT NOT NULL
+
 );
